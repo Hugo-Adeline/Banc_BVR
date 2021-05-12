d49647890277f1115be3ef54abaa75528d070008
Hugo_Adeline <hugoadeline@hotmail.fr>
2021-05-12 11:16:57 +0200

    Ajout de commentaires

--- raspberry/Banc_Robot/Software/GUI/AutoCalib.py
+++ raspberry/Banc_Robot/Software/GUI/AutoCalib.py
@@ -6,11 +6,13 @@ from GUI.GUI_utils import Popup
 
 def AutoCalib(self):
 
-    Popup(self, 1, texte= "Assurez vous que tous les capteurs sont branchés et appuyer sur 'Continuer'", fermer= 'Continuer')
+    # On demande de vérifier le branchement des capteurs
+    Popup(self, 1, texte= "Assurez vous que tous les capteurs sont branchés avant de continuer.", fermer= 'Continuer')
 
+    # On éteint les relais
     self.root.interface.GPIOReset()
 
-    #Récupération de la déviation des capteurs branchés
+    # Récupération de la déviation des capteurs branchés
     for category in self.robotAttributes['Sensors']:
         for subCategory in self.robotAttributes['Sensors'][category]:
             tempList = []
@@ -26,7 +28,7 @@ def AutoCalib(self):
             else:
                 self.robotAttributes['Sensors'][category][subCategory]['Deviation'] = round(variance**(0.5), 2)
 
-    #Calibration du système de pression
+    # Calibration du système de pression
     pressureSensor = self.root.interface.sensorClass['Pression']['Robot']
     pressureActuator = self.root.interface.actuatorClass['Electro_pompe']['Robot']
     evDict = self.robotAttributes['Actuators']['Electrovanne']
@@ -34,9 +36,10 @@ def AutoCalib(self):
     for subCategory in evDict:
     	evClassList.append(self.root.interface.actuatorClass['Electrovanne'][subCategory])
 
-    #Détermination de la pression maximale et minimale
+    # Détermination de la pression maximale et minimale
     timer = 5
     counter = 0
+    # Slection d'un actionneur hydraulique
     if 'Embrayage +' in evDict:
         positionSensor = self.root.interface.sensorClass['Position']['Embrayage 1']
         actuatorUp = self.root.interface.actuatorClass['Electrovanne']['Embrayage +']
@@ -55,6 +58,7 @@ def AutoCalib(self):
         actuatorDown = self.root.interface.actuatorClass['Electrovanne']['Sélection -']
         devSensor = self.robotAttributes['Sensors']['Position']['Sélection 1']['Deviation']
         objective = 20
+    # Récupération de son min et max
     pressureActuator.Set(state = 1)
     sleep(3)
     pressureActuator.Set(state = 0)
@@ -68,17 +72,18 @@ def AutoCalib(self):
     actuatorUp.Set(state= 0)
     sleep(0.5)
     maxSensor = positionSensor.Poll(averaging= 5)
-    #Vidage de la pression
+    # Vidage de la pression
     for i in range(5):
         for ev in evClassList:
             ev.Set(state= 1)
             sleep(0.5)
             ev.Set(state= 0)
             sleep(0.25)
-    #Sauvegarde de la pression minimale
+    # Sauvegarde de la pression minimale
     self.robotAttributes['Sensors']['Pression']['Robot']['Min'] = pressureSensor.Poll()
     print("min capteur = ", minSensor)
     print("max capteur = ", maxSensor)
+    # Augmentation de la pression max jusqu'à ce que l'actionneur effectue le nombre de va et vient ciblé
     while counter < objective:
         counter = 0
         pressureActuator.Set(state = 1)
@@ -111,15 +116,17 @@ def AutoCalib(self):
                 counter +=1
         print('counter = ', counter)
         timer += 1
+    # Vidage de la pression
     for i in range(5):
         for ev in evClassList:
             ev.Set(state= 1)
             sleep(0.5)
             ev.Set(state= 0)
             sleep(0.25)
+    # Récupération du temps que met la pompe pour atteindre la pression max
     self.robotAttributes['Actuators']['Electro_pompe']['Robot']['Time'] = self.root.interface.actuatorClass['Electro_pompe']['Robot'].GetTime(self.robotAttributes)
 
-    #Détermination des min et max capteur position
+    # Détermination des min et max capteur position et de leur type
     for subCategory in self.robotAttributes['Sensors']['Position']:
         pressureActuator.Set(state= 1)
         while self.robotAttributes['Sensors']['Pression']['Robot']['Max'] > pressureSensor.Poll(5):
@@ -132,7 +139,7 @@ def AutoCalib(self):
         pressureActuator.Set(state= 0)
         self.robotAttributes['Sensors']['Position'][subCategory]['Min'], self.robotAttributes['Sensors']['Position'][subCategory]['Max'] = self.root.interface.sensorClass['Position'][subCategory].GetMinMax(self.robotAttributes)
 
-    #Récupération de la déviation des capteurs branchés
+    # Récupération de la déviation des capteurs branchés
     print('début du calcul dev')
     Popup(self, 1, texte= "Veuillez débrancher tous les capteurs et appuyer sur 'Continuer'", fermer= 'Continuer')
     for category in self.robotAttributes['Sensors']:
